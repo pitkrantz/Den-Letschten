791ffd7b067c3323b3224e8e26f752d9486b35b0
pitkrantz <pitkrantz@gmail.com>
2023-06-08 20:51:09 +0200

    Initial Commit

---
+++ Den Letschten/Den_LetschtenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Den_LetschtenApp.swift
+//  Den Letschten
+//
+//  Created by Pit Krantz on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Den_LetschtenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
